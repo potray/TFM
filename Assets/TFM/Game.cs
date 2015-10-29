d18e55c6dccd81801c2c2ad5a5af37648cf6e83b
Daniel <potrayarrick@gmail.com>
2015-10-29 13:17:17 +0100

    Simon says started
    Created the Lego Platform, checking if the model corresponds with the
    real one.

---
+++ Assets/TFM/Game.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class Game : MonoBehaviour {
+
+
+    // Leap motion controller.
+
+    private Controller controller;
+
+    // Gameobjects from scene.
+
+    public GameObject sphere;
+
+    // Use this for initialization
+    void Start () {
+        // Set up controller.
+        controller = new Controller();
+
+        while (!controller.IsConnected) { }
+
+        Debug.Log("Success!");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Get the frame, the right hand and it's index.
+        Frame frame = controller.Frame();
+
+        Hand rightHand = frame.Hands.Rightmost;
+
+        Finger index = rightHand.Fingers.FingerType(Finger.FingerType.TYPE_INDEX)[0];
+
+        // Set the sphere position to the index position.
+        sphere.transform.position = new Vector3(- index.TipPosition.x / 10, index.TipPosition.y / 10, index.TipPosition.z / 10);
+
+    }
+
+
+}
