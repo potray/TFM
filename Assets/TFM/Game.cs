7085c2eb9de4c8d9d6fdccd2d452deb2e9544dc8
Daniel <potrayarrick@gmail.com>
2015-11-04 18:43:45 +0100

    Made a Simon Dice simple game.

--- Assets/TFM/Game.cs
+++ Assets/TFM/Game.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Leap;
+using System.Collections.Generic;
 
 public class Game : MonoBehaviour {
 
@@ -12,6 +13,25 @@ public class Game : MonoBehaviour {
     // Gameobjects from scene.
 
     public GameObject sphere;
+    public GameObject inGameLeapMotion;
+    public GameObject inGamePlatform;
+    public GameObject redLeftHook;
+    public GameObject redRightHook;
+    public GameObject redCenterHook;
+    public GameObject blueLeftHook;
+    public GameObject blueRightHook;
+    public GameObject blueCenterHook;
+
+    public GameObject redLeftStickWithHook;
+    public GameObject redRightStickWithHook;
+    public GameObject redCenterStickWithHook;
+    public GameObject blueLeftStickWithHook;
+    public GameObject blueRightStickWithHook;
+    public GameObject blueCenterStickWithHook;
+
+    // Game attributes.
+    private int lastHook;
+    private List <GameObject> redHooks, blueHooks, redSticks, blueSticks;
 
     // Use this for initialization
     void Start () {
@@ -21,6 +41,32 @@ public class Game : MonoBehaviour {
         while (!controller.IsConnected) { }
 
         Debug.Log("Success!");
+
+        // Set up attributes.
+        lastHook = -1;
+
+        redHooks = new List<GameObject>();
+        redHooks.Add(redLeftHook);
+        redHooks.Add(redRightHook);
+        redHooks.Add(redCenterHook);
+
+        blueHooks = new List<GameObject>();
+        blueHooks.Add(blueLeftHook);
+        blueHooks.Add(blueRightHook);
+        blueHooks.Add(blueCenterHook);
+
+        redSticks = new List<GameObject>();
+        redSticks.Add(redLeftStickWithHook);
+        redSticks.Add(redRightStickWithHook);
+        redSticks.Add(redCenterStickWithHook);
+
+        blueSticks = new List<GameObject>();
+        blueSticks.Add(blueLeftStickWithHook);
+        blueSticks.Add(blueRightStickWithHook);
+        blueSticks.Add(blueCenterStickWithHook);
+
+        // Start the game
+        selectNextHook();
     }
 	
 	// Update is called once per frame
@@ -30,12 +76,57 @@ public class Game : MonoBehaviour {
 
         Hand rightHand = frame.Hands.Rightmost;
 
-        Finger index = rightHand.Fingers.FingerType(Finger.FingerType.TYPE_INDEX)[0];
+        Finger index = rightHand.Fingers.FingerType(Finger.FingerType.TYPE_INDEX)[0];        
 
         // Set the sphere position to the index position.
-        sphere.transform.position = new Vector3(- index.TipPosition.x / 10, index.TipPosition.y / 10, index.TipPosition.z / 10);
+        sphere.transform.position = new Vector3(- index.TipPosition.x / 10, index.TipPosition.y / 10 + inGamePlatform.transform.position.y + inGameLeapMotion.transform.position.y, index.TipPosition.z / 10 + inGameLeapMotion.transform.position.z);
+    }
 
+    void selectNextHook ()
+    {
+        // Generate a valid random number.
+        System.Random rnd = new System.Random();
+        int i = lastHook;
+        while (i == lastHook)
+        {
+            i = rnd.Next(0, 3);
+        }
+
+        Debug.Log(i);
+        
+        // Swap hooks.
+        if (lastHook != -1) // Skip the first previous swap
+        {
+            blueSticks[lastHook].SetActive(false);
+            redSticks[lastHook].SetActive(true);
+        }
+
+        blueSticks[i].SetActive(true);
+        redSticks[i].SetActive(false);
+
+        lastHook = i;
     }
 
 
+    // This is called by the sphere whenever it touches an object.
+    public void HookTouched (GameObject obj)
+    {
+        /*if (obj.Equals(redLeftHook))
+        {
+            Debug.Log("leftHook");
+        }
+        else if (obj.Equals(redRightHook))
+        {
+            Debug.Log("rightHook");
+        }
+        else if (obj.Equals(redCenterHook))
+        {
+            Debug.Log("centerHook");
+        }*/
+
+        if (blueHooks.IndexOf(obj) == lastHook)
+        {
+            selectNextHook();
+        }
+    }
 }
