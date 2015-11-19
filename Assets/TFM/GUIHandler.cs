1f661bc8942d94fb5d23c5e06b998e0e6683ca71
Daniel <potrayarrick@gmail.com>
2015-11-19 12:53:43 +0100

    Added a menu to the platform.

---
+++ Assets/TFM/GUIHandler.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using SimpleJSON;
+
+public class GUIHandler : MonoBehaviour {
+
+    public Text inputText;
+    public Text infoText;
+    public Button logInButton;
+    public Button confirmButton;
+    public Canvas loginCanvas;
+    public Canvas selectGameCanvas;
+    private int playerId;
+
+    public void Start()
+    {
+        HideButton(confirmButton);
+        playerId = -1;
+    }
+
+    public void LogIn()
+    {
+        // Get the text in the input field, check if the code is valid.
+        try
+        {
+            playerId = int.Parse(inputText.text);
+            infoText.text = "";
+
+        }
+        catch (FormatException ex)
+        {
+            infoText.text = "Please enter a valid code.";
+        }
+
+        if (playerId != -1)
+        {
+            //TODO change to the real one
+            string debugUrl = "http://127.0.0.1:8000/patients/validateCode?code=" + playerId;
+            Debug.Log(debugUrl);
+
+            var www = new WWW(debugUrl);
+
+            // Wait for request to complete
+            StartCoroutine(WaitForRequest(www));
+         
+        }
+    }
+
+    public void OnTextInputClick() {
+        infoText.text = "Enter the code your doctor provided above.";
+        HideButton(confirmButton);
+        ShowButton(logInButton);
+    }
+
+    // Save playerprefs and show select game buttons.
+    public void Confirm()
+    {
+        PlayerPrefs.SetInt("PlayerID", playerId);
+        loginCanvas.gameObject.SetActive(false);
+        selectGameCanvas.gameObject.SetActive(true);
+    }
+
+    private void HideButton (Button button)
+    {
+        button.gameObject.SetActive(false);
+    }
+
+    private void ShowButton (Button button)
+    {
+        button.gameObject.SetActive(true);
+    }
+
+    public void LaunchGame (String game)
+    {
+        Application.LoadLevel(game);
+    }
+
+    private IEnumerator WaitForRequest (WWW www)
+    {
+        yield return www;
+
+        if (www.error == null)
+        {
+            Debug.Log(www.text);
+            var json = JSON.Parse(www.text);
+            var firstName = json["first_name"].Value;
+            var lastName = json["last_name"].Value;
+            var ok = json["ok"].AsBool;
+            Debug.Log(firstName + " " + lastName + " " + ok);
+
+            if (ok)
+            {
+                infoText.text = "Loggin in as " + firstName + " " + lastName + ". Please confirm or enter a new code.";
+                HideButton(logInButton);
+                ShowButton(confirmButton);
+            }
+            else
+            {
+                infoText.text = "The code you entered doesn't exists, please try again";
+            }
+
+        }
+        else
+        {
+            Debug.Log("Error! " + www.error);
+        }
+    }
+}
--- Assets/TFM/Game.cs
+++ Assets/TFM/Game.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.IO;
 using Leap;
 using System.Collections.Generic;
+using SimpleJSON;
 
 public class Game : MonoBehaviour {
 
@@ -29,9 +32,20 @@ public class Game : MonoBehaviour {
     public GameObject blueRightStickWithHook;
     public GameObject blueCenterStickWithHook;
 
+    public Text text;
+
     // Game attributes.
-    private int lastHook;
+    private int lastHook, totalHooks, maxHooks;
     private List <GameObject> redHooks, blueHooks, redSticks, blueSticks;
+
+    // Strings
+    private string gameEnding = "Juego terminado";
+
+    // Positions of the fingers
+    private List<double> indexPositions;
+    private List<double> times;
+
+    private double time;
 
     // Use this for initialization
     void Start () {
@@ -44,6 +58,8 @@ public class Game : MonoBehaviour {
 
         // Set up attributes.
         lastHook = -1;
+        maxHooks = 1;
+        totalHooks = -1;
 
         redHooks = new List<GameObject>();
         redHooks.Add(redLeftHook);
@@ -65,6 +81,10 @@ public class Game : MonoBehaviour {
         blueSticks.Add(blueRightStickWithHook);
         blueSticks.Add(blueCenterStickWithHook);
 
+        indexPositions = new List<double>();
+        times = new List<double>();
+        time = 0;
+
         // Start the game
         selectNextHook();
     }
@@ -76,10 +96,20 @@ public class Game : MonoBehaviour {
 
         Hand rightHand = frame.Hands.Rightmost;
 
-        Finger index = rightHand.Fingers.FingerType(Finger.FingerType.TYPE_INDEX)[0];        
+        Pointable index = rightHand.Fingers.FingerType(Finger.FingerType.TYPE_INDEX)[0];
+        
+        //Pointable index = frame.Tools.Rightmost;
 
         // Set the sphere position to the index position.
         sphere.transform.position = new Vector3(- index.TipPosition.x / 10, index.TipPosition.y / 10 + inGamePlatform.transform.position.y + inGameLeapMotion.transform.position.y, index.TipPosition.z / 10 + inGameLeapMotion.transform.position.z);
+
+        // Check if the hand is in Leap's field of vision.
+        if (index.TipPosition.x != 0 && index.TipPosition.y != 0 && index.TipPosition.z != 0 && totalHooks != maxHooks)
+        {
+            time += Time.deltaTime;
+            indexPositions.Add(index.TipPosition.y);
+            times.Add(time);
+        }
     }
 
     void selectNextHook ()
@@ -92,7 +122,8 @@ public class Game : MonoBehaviour {
             i = rnd.Next(0, 3);
         }
 
-        Debug.Log(i);
+        totalHooks++;
+        Debug.Log(totalHooks);
         
         // Swap hooks.
         if (lastHook != -1) // Skip the first previous swap
@@ -101,29 +132,47 @@ public class Game : MonoBehaviour {
             redSticks[lastHook].SetActive(true);
         }
 
-        blueSticks[i].SetActive(true);
-        redSticks[i].SetActive(false);
+        if (totalHooks == maxHooks)
+        {
+            EndGame();
+        }
+        else
+        {
+            blueSticks[i].SetActive(true);
+            redSticks[i].SetActive(false);
 
-        lastHook = i;
+            lastHook = i;
+        }
+    }
+
+    private void EndGame()
+    {
+        text.text = gameEnding;
+
+        using (StreamWriter sr = new StreamWriter("file.txt"))
+        {
+            sr.WriteLine(times.Count);
+
+            foreach(var item in indexPositions)
+            {
+                sr.WriteLine(item);
+            }
+
+            sr.WriteLine("Times");
+            foreach (var item in times)
+            {
+                sr.WriteLine(item);
+            }
+
+        }
+
+
     }
 
 
     // This is called by the sphere whenever it touches an object.
     public void HookTouched (GameObject obj)
     {
-        /*if (obj.Equals(redLeftHook))
-        {
-            Debug.Log("leftHook");
-        }
-        else if (obj.Equals(redRightHook))
-        {
-            Debug.Log("rightHook");
-        }
-        else if (obj.Equals(redCenterHook))
-        {
-            Debug.Log("centerHook");
-        }*/
-
         if (blueHooks.IndexOf(obj) == lastHook)
         {
             selectNextHook();
